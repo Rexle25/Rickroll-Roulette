16d6c08c1cc1d3d3542af268f0ebb009393dc921
Rexle25 <gisbert11@protonmail.com>
2024-12-08 17:13:29 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,20 @@
-// Firebase SDK importieren
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
-import { getFirestore, doc, getDoc, updateDoc, increment } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Firebase-Konfiguration (ersetze mit deinen eigenen Konfigurationsdetails)
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCnrrFUOMjwITTo1znYKerllzlpmLQWAvA",
+  authDomain: "rickroll-roulette.firebaseapp.com",
+  projectId: "rickroll-roulette",
+  storageBucket: "rickroll-roulette.firebasestorage.app",
+  messagingSenderId: "455591156147",
+  appId: "1:455591156147:web:0bc7583656e611d2e793c6"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 // Firebase initialisieren
 const app = initializeApp(firebaseConfig);
